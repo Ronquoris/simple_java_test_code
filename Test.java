db7606ba6e1d8e2bd886308d1248c34ebe223c61
Ronit Sarkar <ronronit1514@gmail.com>
2026-04-01 09:07:14 +0530

    Add test log message to output

--- Test.java
+++ Test.java
@@ -40,6 +40,7 @@ public class Test {
             }
 
             // Print the result for the Jenkins log
+            System.out.println("hello this for test purpose only");
             System.out.println("Calculation: " + num1 + " " + operator + " " + num2);
             System.out.println("Result: " + result);
 
